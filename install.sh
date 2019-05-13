3b235fd80701ebfa07edd9b27c75d9cf7c776613
Guillaume BILLEY <guillaume.billey@pharmagest.com>
2019-05-13 10:54:16 +0200

    Add powerfont and powerlevel9k

--- install.sh
+++ install.sh
@@ -11,4 +11,10 @@ then
   mv ~/.zshrc ~/.zshrc.back
 fi
 
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mv PowerlineSymbols.otf ~/.local/share/fonts/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+fc-cache -vf ~/.local/share/fonts/
 ln -sv ~/.dotfiles/zsh/.zshrc ~/.zshrc
